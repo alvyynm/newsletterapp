e4fe8da2ccb5dbde43526fb494f7d48c070da3af
alvyynm <alvyynm@gmail.com>
2024-01-08 10:57:51 +0300

    chore: add email validator function

---
+++ src/lib/utils.ts
@@ -0,0 +1,8 @@
+// store utility functions
+export const isValidEmail = (email: string | undefined | null): boolean => {
+  if (!email) return false;
+  const regex = new RegExp(
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+  return regex.test(email);
+};
